e8714f9a0cccc5034c24821056d5c117b6bbd8b1
Prasanna Adhikari <prasannaadhikari2602@gmail.com>
2026-05-20 13:32:35 +0545

    build the full crud recipie app

--- RecipeApp/apps/main/urls.py
+++ RecipeApp/apps/main/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.home)
+    path('', views.home, name="recipe_list"),
+    path('add/', views.add , name="add_recipe"),
+    path('edit/<int:ID>/', views.edit ,name="edit_recipe"),
+    path('delete/<int:ID>/', views.delete ,name="delete_recipe"),
 ]
--- RecipeApp/apps/main/views.py
+++ RecipeApp/apps/main/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Recipe
 # Create your views here.
 
@@ -10,3 +10,56 @@ def home(request):
         "Recipes" : recipes
     }
     return render(request, 'home/home.html', context)
+
+def add(request):
+
+    if request.method == "POST":
+
+        title = request.POST.get('title')
+        description = request.POST.get('description')
+        ingredients = request.POST.get('ingredients')
+        instruction = request.POST.get('instruction')
+        total_time = request.POST.get('total_time')
+
+        image = request.FILES.get('image')
+
+        Recipe.objects.create(
+            title=title,
+            description=description,
+            ingredients=ingredients,
+            instruction=instruction,
+            total_time=total_time,
+            image=image
+        )
+
+        return redirect('recipe_list')
+
+    return render(request, 'home/add.html')
+
+
+def edit(request, ID):
+      recipe = Recipe.objects.get(id = ID)
+      if request.method == "POST":
+        recipe.title = request.POST['title']
+        recipe.description = request.POST['description']
+        recipe.ingredients = request.POST['ingredients']
+        recipe.instruction = request.POST['instruction']
+        recipe.total_time = request.POST['total_time']
+
+        # image optional update
+        if 'image' in request.FILES:
+            recipe.image = request.FILES['image']
+
+        recipe.save()
+        return redirect('recipe_list')
+
+      return render(request, 'home/edit.html', {'recipe': recipe})
+  
+  
+def delete(request, ID):
+
+    recipe = Recipe.objects.get(id = ID)
+    recipe.delete()
+    return redirect('recipe_list')
+
+    
