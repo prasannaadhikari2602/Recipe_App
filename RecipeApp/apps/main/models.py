dd538ffd53dbef87bc88ec4a5f719681fc8599a1
Prasanna Adhikari <prasannaadhikari2602@gmail.com>
2026-05-20 12:25:37 +0545

    buit the main section part where data are shown

--- RecipeApp/apps/main/models.py
+++ RecipeApp/apps/main/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Recipe(models.Model):
+    
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    ingredients = models.TextField()
+    instruction = models.TextField()
+    total_time = models.IntegerField()
+    image = models.ImageField(upload_to='.', blank=True, null=True)
+    
--- RecipeApp/apps/main/views.py
+++ RecipeApp/apps/main/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
-
+from .models import Recipe
 # Create your views here.
 
 def home(request):
+    
+    recipes = Recipe.objects.all()
+    
     context = {
-        "title": "Recipio"
+        "Recipes" : recipes
     }
     return render(request, 'home/home.html', context)
